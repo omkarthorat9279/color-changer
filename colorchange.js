f7ff85655f5c99ab60ca4675dbac2b98f9dbe779
omkarthorat9279 <omkarthorat9279@gmail.com>
2025-03-16 18:00:39 +0530

    alert changes

--- colorchange.js
+++ colorchange.js
@@ -1,25 +1,25 @@
 console.log("hitesh")
 const buttons = document.querySelectorAll('.button');
 const body = document.querySelector('body');
+const customAlert = document.getElementById('custom-alert');
+
+function showAlert(message, color) {
+  customAlert.textContent = message;
+  customAlert.style.backgroundColor = color;
+  customAlert.style.display = 'block';
+  setTimeout(() => {
+    customAlert.style.display = 'none';
+  }, 2000); // Hide the alert after 2 seconds
+}
 
 buttons.forEach(function (button) {
   console.log(button);
   button.addEventListener('click', function (e) {
     console.log(e);
     console.log(e.target);
-    if (e.target.id === 'grey') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'white') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'blue') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'yellow') {
-      body.style.backgroundColor = e.target.id;
-    }
-    
+    const color = e.target.id;
+    body.style.backgroundColor = color;
+    showAlert(`You have selected ${color} color`, color);
   });
 });
 
